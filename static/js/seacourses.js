0f5167d1123c3c1963b59adb51761d30dfb04466
Brian <brichen95@gmail.com>
2015-09-05 20:16:43 -0400

    selected classes box up

--- static/js/seacourses.js
+++ static/js/seacourses.js
@@ -195,6 +195,7 @@ function parseCourses(i) {
     var newTableEnd = $('<td>');
     newTableEnd.addClass('tableEnd');
 
+    newTableCheck.append($('<input type="checkbox" id="checkbox_' + obj.id + '" onclick="toggleClass(' + obj.id + ');">'));
     newTableDepartment.append(obj.dept);
     newTableCourseCode.append(obj.code);
     newTableCourseName.append(obj.name);
@@ -216,106 +217,99 @@ function parseCourses(i) {
     newTableRow.append(newTableStart);
     newTableRow.append(newTableEnd);
 
-    $('#bigTable').append($(newTableRow));
-
-    //console.log("yoo");
-
+    $('#bigTable').append(newTableRow);
 }
 
-//
-//$(document).ready(function() {
-//    $('.searchField').hide();
-//
-//    $('#searchBox').change(function() {
-//        var $valueOfDiv= $(this).find('option:selected').val();
-//        $('#' + $valueOfDiv + 'Search').toggle();
-//        $('#searchBox').val('selectDropDown');
-//    });
-//
-//    $('.searchField').hide();
-//
-//    $('#searchBox').change(function() {
-//        var $valueOfDiv= $(this).find('option:selected').val();
-//        $('#' + $valueOfDiv + 'Search').toggle();
-//        $('#searchBox').val('selectDropDown');
-//    });
-//
-//    $('#clearButton').click(function() {
-//        $('.searchField').hide();
-//    });
-//
-//
-//    $('.searchField').hide();
-//
-//    var i;
-//
-//    function parseCourses(i) {
-//        var obj = courses[i];
-//
-//        var newTableRow = $('<tr>');
-//        newTableRow.addClass('tableCourses');
-//        var newTableCheck = $('<td>');
-//        newTableCheck.addClass('tableCheck');
-//        var newTableDepartment = $('<td>');
-//        newTableDepartment.addClass('tableDepartment');
-//        var newTableCourseCode = $('<td>');
-//        newTableCourseCode.addClass('tableCourseCode');
-//        var newTableCourseName = $('<td>');
-//        newTableCourseName.addClass('tableCourseName');
-//        var newTableDEC = $('<td>');
-//        newTableDEC.addClass('tableDEC');
-//        var newTableSBC = $('<td>');
-//        newTableSBC.addClass('tableSBC');
-//        var newTableProfessor = $('<td>');
-//        newTableProfessor.addClass('tableProfessor');
-//        var newTableDays = $('<td>');
-//        newTableDays.addClass('tableDays');
-//        var newTableStart = $('<td>');
-//        newTableStart.addClass('tableStart');
-//        var newTableEnd = $('<td>');
-//        newTableEnd.addClass('tableEnd');
-//
-//        console.log(obj);
-//
-//        newTableDepartment.append(obj.dept);
-//        newTableCourseCode.append(obj.code);
-//        newTableCourseName.append(obj.name);
-//        newTableDEC.append(obj.dec);
-//        newTableSBC.append(obj.sbcs);
-//        newTableProfessor.append(obj.prof);
-//        newTableDays.append(obj.days);
-//        newTableStart.append(obj.start);
-//        newTableEnd.append(obj.end);
-//
-//        newTableRow.append(newTableCheck);
-//        newTableRow.append(newTableDepartment);
-//        newTableRow.append(newTableCourseCode);
-//        newTableRow.append(newTableCourseName);
-//        newTableRow.append(newTableDEC);
-//        newTableRow.append(newTableSBC);
-//        newTableRow.append(newTableProfessor);
-//        newTableRow.append(newTableDays);
-//        newTableRow.append(newTableStart);
-//        newTableRow.append(newTableEnd);
-//
-//        $('#bigTable').append($(newTableRow));
-//
-//        //console.log("yoo");
-//
-//    }
-//
-//    for (i = 0; i < 15; i++) {
-//        parseCourses(i);
-//    }
-//
-//    $('#searchBox').change(function() {
-//        var $valueOfDiv= $(this).find('option:selected').val();
-//        $('#' + $valueOfDiv + 'Search').toggle();
-//        $('#searchBox').val('selectDropDown');
-//    });
-//
-//    $('#clearButton').click(function() {
-//        $('.searchField').hide();
-//    });
-//
-//});
+var courses = [];
+var selectedCourses = [];
+var searchedCourses = [];
+$(document).ready(function() {
+    $.getJSON( '/search', function( data ) {
+        $.each( data, function( key, val ) {
+            courses.push(val);
+        });
+
+        //selectedCourses.push(courses[0]);
+        //selectedCourses.push(courses[1]);
+        //selectedCourses.push(courses[2]);
+        //selectedCourses.push(courses[3]);
+
+        $('.searchField').hide();
+
+        $('#searchBox').change(function() {
+            var $valueOfDiv= $(this).find('option:selected').val();
+            $('#' + $valueOfDiv + 'Search').toggle();
+            $('#searchBox').val('selectDropDown');
+        });
+
+        $('#clearButton').click(function() {
+            $('.searchField').hide();
+        });
+
+        $('.searchField').hide();
+
+        var i;
+        for (i = 0; i < 15; i++) {
+            parseCourses(i);
+        }
+
+        $('#searchBox').change(function() {
+            var $valueOfDiv= $(this).find('option:selected').val();
+            $('#' + $valueOfDiv + 'Search').toggle();
+            $('#searchBox').val('selectDropDown');
+        });
+
+        $('#clearButton').click(function() {
+            $('.searchField').hide();
+        });
+    });
+});
+
+function toggleClass(obj)
+{
+    var course = searchCourseByID(obj);
+    var index = selectedCourses.indexOf(course);
+    if (index != -1)
+        selectedCourses.splice(index, 1);
+    else
+        selectedCourses.push(course);
+    redrawSelectedClasses();
+}
+
+function searchCourseByID(id)
+{
+    for (var i = 0; i < courses.length; i++)
+    {
+        if (courses[i].id == id)
+            return courses[i];
+    }
+    return null;
+}
+
+function redrawSelectedClasses()
+{
+    $.each($('.selectedClassItem'), function(index, value){
+        value.remove();
+    });
+    for (var i = 0; i < selectedCourses.length; i++)
+    {
+        var course = selectedCourses[i];
+        var listElement = $('<li>');
+        var string = '';
+
+        listElement.addClass('list-group-item selectedClassItem');
+        listElement.append($('<button type="button" class="btn" name="' + course.id + '" onclick="removeSelectedCLass(this);">&#10006</button>'));
+        string += course.dept + course.code + ' - ' + course.name;
+        listElement.append(string);
+
+        $('#classList').append(listElement);
+    }
+}
+
+function removeSelectedCLass(obj)
+{
+    var name = obj.name;
+    selectedCourses.splice(selectedCourses.indexOf(searchCourseByID(name)), 1);
+    $('#checkbox_' + name).attr('checked', false);
+    jQuery(obj).parent().remove();
+}
