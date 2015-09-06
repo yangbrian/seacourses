3f811b6d6569b29627c570ca5519cf26ee230b0b
Brian Yang <brian@brianyang.me>
2015-09-05 20:11:39 -0400

    Fix merge conflicts

--- static/js/seacourses.js
+++ static/js/seacourses.js
@@ -2,110 +2,21 @@
  * Main script for SeaCourses
  */
 
-var page = 0;
-//var count;
-//
-//function loadCourses(sort, order) {
-//    var search = document.getElementsByClassName("search-box");
-//    var params = {};
-//    for (var i = 0; i < search.length; i++) {
-//        if (search[i].value != '')
-//            params[search[i].getAttribute("data-type")] = search[i].value;
-//    }
-//
-//    // sort by columns
-//    if (sort) {
-//        params["sort"] = sort;
-//        params["order"] = order;
-//    }
-//
-//    var xhr = new XMLHttpRequest();
-//    xhr.onreadystatechange = function() {
-//        if (xhr.readyState == 4) {
-//            displayTable(xhr.response)
-//        }
-//    };
-//
-//    var queries = "?limit=10&page=" + page;
-//
-//    for (var key in params)
-//        queries += "&" + key + "=" + params[key];
-//
-//    xhr.open('GET', 'fetch.php' + queries, true);
-//
-//    xhr.timeout = 7000;
-//    xhr.ontimeout = function() {
-//        alert("We failed");
-//    };
-//    xhr.send();
-//}
-//
-//document.addEventListener("DOMContentLoaded", initPage);
-//
-//function initPage() {
-//    loadCourses();
-//    var searchBoxes = document.getElementsByClassName("search-box");
-//    for (var i = 0; i < searchBoxes.length; i++) {
-//        console.log("Events");
-//        searchBoxes[i].oninput = function() {
-//            page = 0;
-//            loadCourses();
-//        }
-//    }
-//
-//    document.getElementById("previous-page").onclick = previousPage;
-//    document.getElementById("next-page").onclick = nextPage;
-//}
-//
-//function displayTable(courses) {
-//
-//    courses = JSON.parse(courses);
-//    var table = document.getElementById("course-table");
-//    table.innerHTML = "";
-//
-//    count = courses[0];
-//
-//    var resultCount = document.getElementById("result-count");
-//    resultCount.innerHTML = courses[0] + " total results</a>";
-//
-//    document.getElementById("previous-page").innerHTML = page != 0 ? "Previous" : "";
-//    document.getElementById("next-page").innerHTML = (page + 1) * 10 < count ? "Next" : "";
-//
-//    console.log(page + ", " + (page*10) + " , " + count);
-//
-//    for (var i = 1; i< courses.length; i++) {
-//        var row = document.createElement("div");
-//        row.id = "row-" + i;
-//        row.className = "course-row";
-//
-//        for (var info in courses[i]) {
-//            if (info != "description") {
-//                var cell = document.createElement("div");
-//                cell.id = "cell-" + info + "-" + i;
-//                cell.className = "course-cell";
-//
-//                cell.innerHTML = courses[i][info];
-//                row.appendChild(cell);
-//            }
-//        }
-//
-//        table.appendChild(row);
-//    }
-//}
-//
-//function nextPage() {
-//    page++;
-//    loadCourses();
-//}
-//
-//function previousPage() {
-//    page--;
-//    loadCourses();
-//}
-//
-//
-//
-//
+var courses = [];
+var selectedCourses = [];
+var searchedCourses = [];
+$(document).ready(function() {
+    $.getJSON( '/search', function( data ) {
+        $.each( data, function( key, val ) {
+            courses.push(val);
+        });
+        
+        selectedCourses.push(courses[0]);
+        selectedCourses.push(courses[1]);
+        selectedCourses.push(courses[2]);
+        selectedCourses.push(courses[3]);
+    });
+});
 
 function Schedule() {
 
@@ -122,10 +33,39 @@ function Schedule() {
 
     $.each(selectedCourses, function(index, value) {
         // calculate blocks
-        var startTime = value.start.match(/(\d{1,2}):(\d{2}) ([ap]m)/);
+        var startMatches = value.start.match(/(\d{1,2}):(\d{2})([AP]M)/);
+        var startRow = ((
+            (parseInt(startMatches[1]) + ((startMatches[3] == 'AM' && parseInt(startMatches[1]) == 12)
+            || (startMatches[3] == 'PM' && parseInt(startMatches[1]) != 12) ? 12 : 0))
+                + (startMatches[2] == '00' ? 0 :.5)
+            ) - 8) * 2;
 
+        var endMatches = value.end.match(/(\d{1,2}):(\d{2})([AP]M)/);
+        var endRow = ((
+                (parseInt(endMatches[1]) + ((endMatches[3] == 'AM' && parseInt(endMatches[1]) == 12)
+                || (endMatches[3] == 'PM' && parseInt(endMatches[1]) != 12) ? 12 : 0))
+                + (endMatches[2] == '00' ? 0 :.5)
+            ) - 8) * 2;
+        var columns = [];
 
+        var days = value.days;
+        if (days.indexOf('SU') > -1)
+            columns.push(0);
+        if (days.indexOf('M') > -1)
+            columns.push(1);
+        if (days.indexOf('TU') > -1)
+            columns.push(2);
+        if (days.indexOf('W') > -1)
+            columns.push(3);
+        if (days.indexOf('TH') > -1)
+            columns.push(4);
+        if (days.indexOf('F') > -1)
+            columns.push(5);
+        if (days.indexOf('S') > -1)
+            columns.push(6);
     });
+
+
 
 }
 
@@ -282,104 +222,100 @@ function parseCourses(i) {
 
 }
 
-var courses = [];
-var selectedCourses = [];
-var searchedCourses = [];
-$(document).ready(function() {
-    $.getJSON( '/search', function( data ) {
-        $.each( data, function( key, val ) {
-            courses.push(val);
-        });
-
-        selectedCourses.push(courses[0]);
-        selectedCourses.push(courses[1]);
-        selectedCourses.push(courses[2]);
-        selectedCourses.push(courses[3]);
-
-        $('.searchField').hide();
-
-        $('#searchBox').change(function() {
-            var $valueOfDiv= $(this).find('option:selected').val();
-            $('#' + $valueOfDiv + 'Search').toggle();
-            $('#searchBox').val('selectDropDown');
-        });
-
-        $('#clearButton').click(function() {
-            $('.searchField').hide();
-        });
-
-
-        $('.searchField').hide();
-
-        var i;
-
-        function parseCourses(i) {
-            var obj = courses[i];
-
-            var newTableRow = $('<tr>');
-            newTableRow.addClass('tableCourses');
-            var newTableCheck = $('<td>');
-            newTableCheck.addClass('tableCheck');
-            var newTableDepartment = $('<td>');
-            newTableDepartment.addClass('tableDepartment');
-            var newTableCourseCode = $('<td>');
-            newTableCourseCode.addClass('tableCourseCode');
-            var newTableCourseName = $('<td>');
-            newTableCourseName.addClass('tableCourseName');
-            var newTableDEC = $('<td>');
-            newTableDEC.addClass('tableDEC');
-            var newTableSBC = $('<td>');
-            newTableSBC.addClass('tableSBC');
-            var newTableProfessor = $('<td>');
-            newTableProfessor.addClass('tableProfessor');
-            var newTableDays = $('<td>');
-            newTableDays.addClass('tableDays');
-            var newTableStart = $('<td>');
-            newTableStart.addClass('tableStart');
-            var newTableEnd = $('<td>');
-            newTableEnd.addClass('tableEnd');
-
-            newTableDepartment.append(obj.dept);
-            newTableCourseCode.append(obj.code);
-            newTableCourseName.append(obj.name);
-            newTableDEC.append(obj.dec);
-            newTableSBC.append(obj.sbcs);
-            newTableProfessor.append(obj.prof);
-            newTableDays.append(obj.days);
-            newTableStart.append(obj.start);
-            newTableEnd.append(obj.end);
-
-            newTableRow.append(newTableCheck);
-            newTableRow.append(newTableDepartment);
-            newTableRow.append(newTableCourseCode);
-            newTableRow.append(newTableCourseName);
-            newTableRow.append(newTableDEC);
-            newTableRow.append(newTableSBC);
-            newTableRow.append(newTableProfessor);
-            newTableRow.append(newTableDays);
-            newTableRow.append(newTableStart);
-            newTableRow.append(newTableEnd);
-
-            $('#bigTable').append($(newTableRow));
-
-            //console.log("yoo");
-
-        }
-
-        for (i = 0; i < 15; i++) {
-            parseCourses(i);
-        }
-
-        $('#searchBox').change(function() {
-            var $valueOfDiv= $(this).find('option:selected').val();
-            $('#' + $valueOfDiv + 'Search').toggle();
-            $('#searchBox').val('selectDropDown');
-        });
-
-        $('#clearButton').click(function() {
-            $('.searchField').hide();
-        });
-
-    });
-
-});
+//
+//$(document).ready(function() {
+//    $('.searchField').hide();
+//
+//    $('#searchBox').change(function() {
+//        var $valueOfDiv= $(this).find('option:selected').val();
+//        $('#' + $valueOfDiv + 'Search').toggle();
+//        $('#searchBox').val('selectDropDown');
+//    });
+//
+//    $('.searchField').hide();
+//
+//    $('#searchBox').change(function() {
+//        var $valueOfDiv= $(this).find('option:selected').val();
+//        $('#' + $valueOfDiv + 'Search').toggle();
+//        $('#searchBox').val('selectDropDown');
+//    });
+//
+//    $('#clearButton').click(function() {
+//        $('.searchField').hide();
+//    });
+//
+//
+//    $('.searchField').hide();
+//
+//    var i;
+//
+//    function parseCourses(i) {
+//        var obj = courses[i];
+//
+//        var newTableRow = $('<tr>');
+//        newTableRow.addClass('tableCourses');
+//        var newTableCheck = $('<td>');
+//        newTableCheck.addClass('tableCheck');
+//        var newTableDepartment = $('<td>');
+//        newTableDepartment.addClass('tableDepartment');
+//        var newTableCourseCode = $('<td>');
+//        newTableCourseCode.addClass('tableCourseCode');
+//        var newTableCourseName = $('<td>');
+//        newTableCourseName.addClass('tableCourseName');
+//        var newTableDEC = $('<td>');
+//        newTableDEC.addClass('tableDEC');
+//        var newTableSBC = $('<td>');
+//        newTableSBC.addClass('tableSBC');
+//        var newTableProfessor = $('<td>');
+//        newTableProfessor.addClass('tableProfessor');
+//        var newTableDays = $('<td>');
+//        newTableDays.addClass('tableDays');
+//        var newTableStart = $('<td>');
+//        newTableStart.addClass('tableStart');
+//        var newTableEnd = $('<td>');
+//        newTableEnd.addClass('tableEnd');
+//
+//        console.log(obj);
+//
+//        newTableDepartment.append(obj.dept);
+//        newTableCourseCode.append(obj.code);
+//        newTableCourseName.append(obj.name);
+//        newTableDEC.append(obj.dec);
+//        newTableSBC.append(obj.sbcs);
+//        newTableProfessor.append(obj.prof);
+//        newTableDays.append(obj.days);
+//        newTableStart.append(obj.start);
+//        newTableEnd.append(obj.end);
+//
+//        newTableRow.append(newTableCheck);
+//        newTableRow.append(newTableDepartment);
+//        newTableRow.append(newTableCourseCode);
+//        newTableRow.append(newTableCourseName);
+//        newTableRow.append(newTableDEC);
+//        newTableRow.append(newTableSBC);
+//        newTableRow.append(newTableProfessor);
+//        newTableRow.append(newTableDays);
+//        newTableRow.append(newTableStart);
+//        newTableRow.append(newTableEnd);
+//
+//        $('#bigTable').append($(newTableRow));
+//
+//        //console.log("yoo");
+//
+//    }
+//
+//    for (i = 0; i < 15; i++) {
+//        parseCourses(i);
+//    }
+//
+//    $('#searchBox').change(function() {
+//        var $valueOfDiv= $(this).find('option:selected').val();
+//        $('#' + $valueOfDiv + 'Search').toggle();
+//        $('#searchBox').val('selectDropDown');
+//    });
+//
+//    $('#clearButton').click(function() {
+//        $('.searchField').hide();
+//    });
+//
+//});
