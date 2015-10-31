1a1542765a79017ecebf2ab19827feed1183f2db
Brian Yang <brian@brianyang.me>
2015-10-30 21:29:58 -0400

    Generate a different border color based on the dept code

--- classes/course.py
+++ classes/course.py
@@ -1,5 +1,6 @@
 __author__ = 'brian'
 
+
 class Course:
     def __init__(self, course):
         self.id = course['_id']
@@ -15,3 +16,13 @@ class Course:
         self.days = course['days']
         self.dec = course['dec']
         self.sbcs = course['sbcs']
+        self.color = self.generateColor(self.dept + self.dept)
+
+    def generateColor(self, dept):
+        hashcode = 0
+
+        for i in dept:
+            hashcode = ord(i) + ((hashcode << 5) - hashcode)
+
+        color = "{0:x}".format((hashcode & 0x00FFFFFF))
+        return "00000"[0:6 - len(color)] + color
